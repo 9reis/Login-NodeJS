d54b4070b4148d9b546718fa61069db9ab38198a
Lucas Reis <lucasreis_cod@hotmail.com>
2022-06-18 15:13:12 -0300

    Criado a rota de login com autenticação via token e criada a rota protegida com validação do tokem

--- app.js
+++ app.js
@@ -19,11 +19,42 @@ app.get('/', (req,res) => {
     res.status(200).json({msg: "Bem vindo a nossa API" })
 })
 
+////////// PRIVATE ROUTE ////////// 
+app.get('/user/:id', checkToken , async (req, res) => {
+    const id = req.params.id
+ 
+    ////  CHECK IF USER EXISTS  ///
+    const user = await User.findById(id,'-password')
+
+    if(!user){
+        return res.status(404).json({ msg: "Usuário não encontrado" })
+    }
+    res.status(200).json({ user })
+})
+
+function checkToken(req,res,next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(" ")[1]
+
+    if(!token){
+        return res.status(401).json({msn: "Acesso negado"})
+    }
+
+    try{
+        const secret = process.env.SECRET
+        jwt.verify(token, secret)
+
+        next()
+    }catch(error){
+        res.status(400).json({msg: "Token invalido!"})
+    }
+}
+
 ////////// REGISTER USER //////////
 app.post('/auth/register', async(req,res) =>{
     const {name, email, password, confirmpassword} = req.body
 
-    ////  VALIDAÇÂO  ////
+    ////  VALIDAÇÃO  ////
     if(!name){
         return res.status(422).json({msg: 'O nome é obrigatório!'})
     }
@@ -66,6 +97,54 @@ app.post('/auth/register', async(req,res) =>{
     }
 })
 
+////////// LOGIN USER //////////
+
+app.post("/auth/login", async(req,res) => {
+    const {email,password} = req.body
+
+    ////  VALIDAÇÃO  ////
+
+    if(!email){
+        return res.status(422).json({msg: "O email é obrigatório"})
+    }
+
+    if(!password){
+        return res.status(422).json({msg: "A senha é obrigatória"})
+    }
+    ///  CHECK IF USER EXISTS  ////
+    const user = await User.findOne({email:email})
+
+    if(!user){
+        return res.status(404).json({msg: "Usuário não encontrado"})
+    }
+
+    ///  CHECK IF PASSWORD MATCH  ////
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if(!checkPassword){
+        return res.status(422).json({msg: "Senha invalida!"})
+    }    
+
+    try{
+        const secret = process.env.SECRET
+        const token = jwt.sign(
+            {
+            id:user._id,
+            },
+            secret, 
+        )
+
+        res.status(200).json({msg: "Autenticação realizada com sucesso" , token })
+
+    }catch(error){
+        console.log(error)
+
+        res.status(500).json({
+            msg : "Aconteceu um erro no servidor"})
+    }
+
+})
+
 ////////// CEDENCIALS //////////
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASS
@@ -74,7 +153,7 @@ mongoose.connect(
     `mongodb+srv://${dbUser}:${dbPassword}@cluster0.uorn7.mongodb.net/?retryWrites=true&w=majority` ,
     ).then(()=>{
     app.listen(3000)
-    console.log('CONECTADO AO BANCO')
+    console.log(' >>>>>>>>>> CONECTADO AO BANCO <<<<<<<<<< ' )
 }).catch((err) =>{
     console.log(err)
 }) 
